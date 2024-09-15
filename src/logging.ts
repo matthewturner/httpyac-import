a0126f1c719e390b5268c6b4d84223f27fc236b1
Matt Turner <matt@fiverings.uk>
2024-09-15 14:13:30 +0100

    Support purge target

--- src/logging.ts
+++ src/logging.ts
@@ -6,10 +6,15 @@ export const rootLogger = new Logger<ILogObj>({ type: 'hidden' });
 rootLogger.attachTransport((logObj: ILogObj) => {
     const meta = <IMeta>logObj._meta;
     switch (meta.logLevelId) {
+        case 0:
+            console.log(chalk.whiteBright(logObj[0]));
+            break;
         case 3:
+            console.log('');
             console.log(chalk.blueBright(logObj[0]));
             break;
         case 4:
+            console.log('');
             console.log(chalk.yellowBright(logObj[0]));
             break;
         default:
